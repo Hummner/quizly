6c1439e577f9c5a6d779631ea3c9e5885efcf1e4
Hummner <bence.cservenyak@gmail.com>
2025-12-29 08:09:23 +0100

    add: CORS Header

--- core/settings.py
+++ core/settings.py
@@ -25,12 +25,21 @@ SECRET_KEY = 'django-insecure-2de9om(pehy(0k_on^#-_#v*j8kq(5+#58e=z#2cftj4#fe9p%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1'
+]
 
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5500",
+]
+
+CORS_ALLOW_CREDENTIALS = True
 
 # Application definition
 
 INSTALLED_APPS = [
+    "corsheaders",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,6 +53,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
