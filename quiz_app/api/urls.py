a6cb482369fe4acd877649c22106c54abac68f70
Hummner <bence.cservenyak@gmail.com>
2025-12-30 16:21:27 +0100

    fix: routers by quiz_app.urls

--- quiz_app/api/urls.py
+++ quiz_app/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import QuziCreateView, QuizzesViewset
 from rest_framework.routers import SimpleRouter
 
-router = SimpleRouter
+router = SimpleRouter()
 router.register(r"quizess", QuizzesViewset, basename="quizess")
 
 urlpatterns = router.urls
